2cd54e959214a8af9eaaf281863acb839230f5cf
smolka <smolka@ps.uni-saarland.de>
2021-12-15 12:42:55 +0100

    green

--- ml/parser.ml
+++ ml/parser.ml
@@ -31,11 +31,10 @@ let lex s : token list =
       | '-' -> if verify (i+1) '>'
           then lex (i+2) (ARR::l)
           else lex (i+1) (SUB::l)
-      | c ->
-          if whitespace c then lex (i+1) l
-          else if digit c then lex_num (i+1) (num c) l
-          else if lc_letter c then lex_id (i+1) 1 l
-          else failwith "lex: illegal character"
+      | c when whitespace c -> lex (i+1) l
+      | c when digit c -> lex_num (i+1) (num c) l
+      | c when lc_letter c -> lex_id (i+1) 1 l
+      | c -> failwith "lex: illegal character"
   and lex_num i n l =
     if exhausted i then lex_num' i n l
     else let c = get i in
